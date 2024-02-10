14db3b00e945e97ed17b8034859f7d818511be64
Prince <Princejodhani21@gmail.com>
2024-02-10 13:34:25 +0530

    bridge info panel works!

--- src/app/bridge-info-panel/bridge-info-panel.component.ts
+++ src/app/bridge-info-panel/bridge-info-panel.component.ts
@@ -1,6 +1,12 @@
 import { Component , Input} from '@angular/core';
 import { Bridge } from '../bridge';
 
+
+function prettyPrintDimention(value: number | null): string{
+  return value ? `${value} m`: 'unknown';
+}
+
+
 @Component({
   selector: 'app-bridge-info-panel',
   templateUrl: './bridge-info-panel.component.html',
@@ -9,6 +15,19 @@ import { Bridge } from '../bridge';
 export class BridgeInfoPanelComponent {
 
  @Input() bridge!: Bridge;
+
+ width(): string{
+  return prettyPrintDimention(this.bridge.width)
+ }
+
+ length(): string{
+  return prettyPrintDimention(this.bridge.length)
+ }
+
+ age(): number{
+  const currentYear = (new Date()).getFullYear();
+  return currentYear - this.bridge.year;
+ }
 
 
 }
